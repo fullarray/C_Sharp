0194cabaaf047f3c09b111858d79daba202b1be8
Jonathan <sifreed77@gmail.com>
2019-11-06 23:06:41 -0500

    Update Program.cs

--- parsers/jsonParse/Program.cs
+++ parsers/jsonParse/Program.cs
@@ -4,10 +4,8 @@ using System;
 namespace jsonParse
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            var exampel1 = @"{""name"": ""Jane Doe"",""age"":18}";
+    {        static void Main(string[] args)
+        {            var exampel1 = @"{""name"": ""Jane Doe"",""age"":18}";
 			var JSONObj = new JavaScriptSerializer().Deserialize<Dictionary<string, string>> (example1);
 			console.WriteLine("Name: "+ JSONObj["name"]);
 			console.WriteLine("Age: "+ JSONObj["age"]);
